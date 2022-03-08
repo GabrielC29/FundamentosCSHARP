6c9a38b74412b0cba9fc2d9632cebcf2d07a14cc
Piero Criales <55029565+GabrielC29@users.noreply.github.com>
2022-03-08 14:49:29 -0500

    GET, POST, DELETE, UPDATE en Servicios Web por HTTP

--- FundamentosCSHARP/Program.cs
+++ FundamentosCSHARP/Program.cs
@@ -9,21 +9,38 @@ namespace FundamentoCSHARP
     {
         static async Task Main(string[] args)
         {
-            string url = "https://jsonplaceholder.typicode.com/posts";
+            string url = "https://jsonplaceholder.typicode.com/posts/20"; //POST, DELETE, UPDATE
+            string url2 = "https://jsonplaceholder.typicode.com/posts"; //GET
+
             HttpClient client = new HttpClient();
 
-            var httpResponse = await client.GetAsync(url);
+            Post post = new Post()
+            {
+                userId = 22,
+                body = "Habla p loco",
+                title = "Saludazo"
+            };
+
+            var data = JsonSerializer.Serialize<Post>(post);
+            HttpContent content = new StringContent(data,System.Text.Encoding.UTF8,"application/json");
+
+            var httpResponse = await client.PutAsync(url,content);
+
+            var httpResponse2 = await client.GetAsync(url2);
 
             if (httpResponse.IsSuccessStatusCode)
             {
-                var json = await httpResponse.Content.ReadAsStringAsync();
-                List<Post> post = JsonSerializer.Deserialize<List<Post>>(json);
+                var result = await httpResponse.Content.ReadAsStringAsync();
 
-                foreach(var item in post)
-                {
-                    Console.WriteLine("id: "+item.id+"\n title: "+item.title+"\n body: "+item.body+"\n ");
-                }
+                var result2 = await httpResponse2.Content.ReadAsStringAsync();
 
+                var postre = JsonSerializer.Deserialize<Post>(result);
+
+                List<Post> posts = JsonSerializer.Deserialize<List<Post>>(result2);
+
+                // var postResult = JsonSerializer.Deserialize<Post>(result);
+
+                Console.WriteLine(postre.title);
             }
 
         }
